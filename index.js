789412cd50da171f961bd71615bcfbf504dc236e
Odil-web <116338011+odik5222@users.noreply.github.com>
2022-10-25 23:08:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+/* let str = "aaa@bbb@ccc"
+console.log(
+    str.replace('@', '!')
+        .replace('@', '!')
+)
+
+
+
+
+let txt = "HELLOWORLD"
+console.log(
+    txt.toLowerCase() 
+ .charAt('h').toUpperCase()
+
+)
+
+
+
+let text = 'HELLO, it is HTML'
+console.log(
+    text.replace('HTML', 'not JS')
+)
+
+
+
+
+let a = "alex"
+console.log(
+    a[0].toUpperCase() + a.slice(1)
+)
+
+
+let dot = "dot.replace(.)"
+
+
+
+console.log(
+    Math.random()
+
+)
+
+     
+     let text_one = "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores praesentium vel soluta, adipisci fuga laudantium, repudiandae sunt sed necessitatibus non officiis perspiciatis quibusdam suscipit ea repellendus accusamus iure doloremque asperioresk?"
+
+
+     console.log(
+        text[text.search('o')] +
+        text[text.search('d')] +
+        text[text.search('i')] +
+        text[text.search('l')] +
+        text[text.search('b')] +
+        text[text.search('e')] +
+        text[text.search('k')]
+     ); */
+
+
+
+
+
+let int = prompt('любое число')
+if (int % 2 === 1) {
+    console.log("нечислительное");
+} else {
+    console.log("числительное");
+}
+
+//2
+
+let name = prompt('номат чи туя')
+if(name >= "O" + "O" + "o" + "о" ){
+    console.log('you can come in restaurant');
+} else {
+    console.log('go home please');
+}
+
+
+
+//3
+
+let age = prompt('Чанба ту')
+if(age >= '18') {
+    console.log('You can come in');
+} else {
+    console.log('go go go go home please baby');
+}
